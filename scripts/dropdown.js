51ec7d175ac46b77baa612934b2869754161eaa0
pierregaly33 <galy.pierre31@gmail.com>
2024-02-01 11:22:07 +0100

    navigation lightbox avec les fleches

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -1,8 +1,12 @@
-const boutonDropdown = document.querySelectorAll(".bouton_dropdown");
-boutonDropdown.forEach((element) => {
-    const input = document.createElement("input");
-    element.appendChild(input);
-});
+const list = document.querySelector(".dropdown_liste");
+list.style.display = "none";
 
-function dropdownOpen() {}
-dropdownOpen();
+const rechercheIngredients = document.querySelector(".dropdown_recherche");
+rechercheIngredients.style.display = "none";
+
+const boutonDropdown = document.querySelector(".bouton_dropdown");
+boutonDropdown.addEventListener("click", () => {
+    list.style.display = "block";
+    rechercheIngredients.style.display = "block";
+});
+console.log(boutonDropdown);
