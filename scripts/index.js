94ee42efaedbce92b589386fd23c1936c9b77b1c
pierregaly33 <galy.pierre31@gmail.com>
2024-03-14 15:45:03 +0100

    ajout recette introuvable

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,11 @@ function displayRecipes(recettes) {
     const mainCards = document.querySelector(".main_cards");
     mainCards.innerHTML = "";
 
+    if (recettes.length == 0) {
+        let div = document.createElement("div");
+        mainCards.appendChild(div);
+        div.textContent = "Recette Introuvable";
+    }
     recettes.forEach((recipe) => {
         const domRecipe = getRecipeDom(recipe);
         mainCards.appendChild(domRecipe);
