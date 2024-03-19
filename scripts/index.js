892e614d4215e36882df9e6b65ddbd0b285a3db6
pierregaly33 <galy.pierre31@gmail.com>
2024-03-19 15:59:53 +0100

    modification recette introuvable

--- scripts/index.js
+++ scripts/index.js
@@ -4,12 +4,15 @@ function displayRecipes(recettes) {
 
     if (recettes.length == 0) {
         let div = document.createElement("div");
+        div.setAttribute("class", "recette-introuvable");
+        mainCards.classList.add("introuvable");
         mainCards.appendChild(div);
         div.textContent = "Recette Introuvable";
     }
     recettes.forEach((recipe) => {
         const domRecipe = getRecipeDom(recipe);
         mainCards.appendChild(domRecipe);
+        mainCards.classList.remove("introuvable");
     });
 }
 
